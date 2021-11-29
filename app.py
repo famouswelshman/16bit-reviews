9b33262ee82daf07157c43541879e04681ffa03a
James Hunt <famouswelshman@yahoo.co.uk>
2021-11-29 11:02:29 +0000

    Profile page added for user successfully logged in

--- app.py
+++ app.py
@@ -60,7 +60,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Hi, {}".format(request.form.get("username")))
+                    flash("Hi, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invalid password entered
                 flash("Incorrect Username and/or Password")
@@ -72,6 +75,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Gets the session username from MongoDB
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
