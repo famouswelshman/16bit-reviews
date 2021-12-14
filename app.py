33c9e1a5e416fc04f51cea52c457ab5bd7695053
James Hunt <famouswelshman@yahoo.co.uk>
2021-12-14 10:04:57 +0000

    updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
-
+    import env
+    
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
