4aad6bb6d9922812a3cbbfe6296f280109f6a85b
James Hunt <famouswelshman@yahoo.co.uk>
2021-12-14 17:15:30 +0000

    updated

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
-    
+  
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
@@ -191,4 +191,4 @@ def user_reviews():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
